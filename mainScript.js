b766948059b208f0550e969230fc94df333091fb
Amy <amaice8@gmail.com>
2024-02-26 01:50:54 -0600

    big changes to the main page omg

--- mainScript.js
+++ mainScript.js
@@ -1,9 +1,29 @@
 ﻿$(document).ready(function () {
-
     //page 1
     $(".close").on("click", function () {
         $(".cookie-banner").fadeOut(750);
         console.log("Cookie banner closed");
     });
-
+    
+    // submenu
+    $('#nav li').hover(
+    function () {
+        //show submenu
+        $('ul', this).slideDown("fast");
+    }, function () {
+        //hide submenu
+        $('ul', this).slideUp("fast");
+    });
+    
+    $('#nav > li > ul > li').hover(
+    function () {
+        //show submenu
+        $('ul', this).slideDown("fast");
+    }, function () {
+        //hide submenu
+        $('ul', this).slideUp("fast");
+    });
+    
+    //update year "copyright"
+    document.getElementById("year").innerHTML = new Date().getFullYear();
 })
