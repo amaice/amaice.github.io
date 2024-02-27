28670edb8fe32999981605befc4491dc526727ed
Amy <amaice8@gmail.com>
2024-02-27 00:17:23 -0600

    update folder heirarchy and add old website
    just dont remove any media teehee

---
+++ oldPage/mainScript.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+
+    //page 1
+    $(".close").on("click", function () {
+        $(".cookie-banner").fadeOut(750);
+        console.log("Cookie banner closed");
+    });
+
+})
