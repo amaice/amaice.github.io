f87156f4b805376b4f570ba4afcc0a1db5badb5c
Amy Gonzalez <amaice8@gmail.com>
2025-03-30 18:12:16 -0500

    jazz chordle, stylize and improve win screen

--- games/Chordle/scripts/mainScript.js
+++ games/Chordle/scripts/mainScript.js
@@ -6,24 +6,25 @@ guesses = [
 	[]
 ];
 
+// chords must be written in flats (no Cb or Fb)
 answers = [
-	['F3','C4','G4','A4','E5'],	// Fmaj9 very open voicing - spirited away
-	['B3', 'Bb4', 'Db5', 'Eb5', 'Gb5'],	// Bmaj9 - giant steps
-	['C3', 'G3', 'D4', 'A4', 'E5'],	// stack of fifths
-	['Db3', 'G3', 'F4', 'B4', 'Eb5'],	// Db9b5
-	['E3', 'A3', 'D4', 'G4', 'B4'],	// Em11 - famous so what voicing
-	['Bb3', 'Eb4', 'C5', 'F5', 'A5'],	// Eb7#11
-	['F3', 'A3', 'Eb4', 'Ab4', 'Db5'],	// F7(#9, b13)
-	['Ab3', 'Db4', 'Gb4', 'B4', 'E5'],	// stack of fourths
-	['E3', 'G4', 'B4', 'Eb5', 'Gb5'],	// EmM9 - james bond
-	['Eb3', 'G4', 'Bb4', 'Db5', 'E5'],	// E7b9
-	['Ab4', 'Db5', 'Eb5', 'G5', 'Bb5'], // Am9sus4
-	['Gb3', 'E4', 'Ab4', 'Bb4', 'D5'],	// Gb9#5
-	['Db3', 'Bb4', 'E4', 'G4', 'C5'],	// Petrushka chord but without the bass note cause it's a 6 note chord lol
-	['G3', 'F4', 'A4', 'C5', 'E5'],	// G13 - one of my favorite chords ever	
-	['D4', 'F4', 'Ab4', 'C5', 'E5'], // Dm9b5
-	['C3', 'Bb3', 'E4', 'Ab4', 'D5'],	// C9#5
-	['G3', 'Bb3', 'Db4', 'F4', 'Gb5']	// the jp chord (Gm7b5b9)
+	['F3' ,	'C4' ,	'G4' ,	'A4' ,	'E5' 	,"The famous Fmaj9 from spirited away"],
+	['B3' ,	'Bb4',	'Db5',	'Eb5',	'Gb5' 	,"Bmaj9 - giant steps"],
+	['C3' ,	'G3' , 	'D4' , 	'A4' , 	'E5' 	,"C6/9 - stack of fifths"],
+	['Db3', 'G3' , 	'F4' , 	'B4' , 	'Eb5' 	,"Db9b5"],
+	['E3' ,	'A3' , 	'D4' , 	'G4' , 	'B4' 	,"Em11 - so what voicing"],
+	['Bb3', 'Eb4',	'C5' , 	'F5' , 	'A5' 	,"Eb7#11"],
+	['F3' ,	'A3' , 	'Eb4',	'Ab4',	'Db5' 	,"F7(#9, b13)"],
+	['Ab3', 'Db4',	'Gb4',	'B4' , 	'E5' 	,"Abm11? - Stack of fourths"],
+	['E3' ,	'G4' , 	'B4' , 	'Eb5',	'Gb5' 	,"EmM9 - James Bond"],
+	['Eb3', 'G4' , 	'Bb4',	'Db5',	'E5' 	,"E7b9"],
+	['Ab4', 'Db5',	'Eb5',	'G5' , 	'Bb5' 	,"Am9sus4"],
+	['Gb3', 'E4' , 	'Ab4',	'Bb4',	'D5' 	,"Gb9#5"],
+	['Gb3', 'Bb3',	'E4' , 	'G4' , 	'C5' 	,"C/F# or F#7(b9,#11)- Petrushka chord (but without the C# cause it's actually a 6 note chord lol)"],
+	['G3' ,	'F4' , 	'A4' , 	'C5' , 	'E5' 	,"G13"],
+	['D4' ,	'F4' , 	'Ab4',	'C5' , 	'E5' 	,"Dm9b5"],
+	['C3' ,	'Bb3',	'E4' , 	'Ab4',	'D5' 	,"C9#5"],
+	['G3' ,	'Bb3',	'Db4',	'F4' , 	'Gb5' 	,"Gm7b5b9"]
 	//['', '', '', '', '']	
 ];
 
@@ -36,11 +37,17 @@ var differenceInTime = today.getTime() - dayOfLaunch.getTime();
 var differenceInDays = Math.floor(differenceInTime / (1000 * 3600 * 24));
 
 // if 4 days since launch, choose 4th chord, use modulus to through chords infinitely
-var currentAnswer = differenceInDays % answers.length;
+var currentAnswer = 12
 var x = 0;
 var y = 0;
 
 var COLUMNS = 5;
+
+answer = "";
+let temp_answer;
+temp_answer = answers[currentAnswer].slice();	// shallow copy!
+temp_answer.pop() // remove comment
+answer = temp_answer.toString();
 
 var colorCorrect = '#55c555';
 var colorRightOctave = '#55cccc';
@@ -130,11 +137,12 @@ function submit(){
 					key.style.fill = colorWrong;
 				});
 			}
-		} 
-		
+		}
+
+		let s = guesses[y].toString()
 		// if all correct
-		if(guesses[y].toString() == answers[currentAnswer].toString()){
-			win();
+		if(guesses[y].toString() === answer){
+			win(answers[currentAnswer][5]);
 			y = 99;	// set out of bounds because u won
 		}
 		x = 0;
@@ -142,59 +150,13 @@ function submit(){
 	}
 }
 
-function win(){
-	alert("yay u win");
+function win(comment){
+	alert("You got it! That chord was: " + comment);
 }
 
-document.getElementById("share").addEventListener("click", share);
-function share(){
-	var msg = "";
-	
-	var square_list = document.querySelectorAll('.roundedSquare');
-	var square_array = [...square_list]; // convert to array
-	square_array.forEach(square => {
-		let color = square.style.color;
-		if(color == colorCorrect){
-			msg = msg.concat('gren');
-			alert("gren");
-		}
-		
-		/*
-		switch (square){
-			case colorCorrect:
-				msg = msg.concat('gren');
-				alert("gren");
-				break;
-			default:
-				msg += "gray";
-		}
-		*/
-	});
-	alert(msg);
 
-	
-	/*
-	document.querySelectorAll('.roundedSquare').forEach(item => {
-		if(item.style.background == colorCorrect){
-			alert("green");
-		}
-		
-		
-		switch (item){
-			case colorCorrect:
-				msg += 'gren';
-				alert("gren");
-				break;
-			default:
-				msg += "gray";
-		}
-	});
-	*/
-	
-}
-
-document.getElementById("flip").addEventListener("click", flip);
-function flip(){
+document.getElementById("respell").addEventListener("click", respell);
+function respell(){
 	var note = document.getElementById((y + 1) + '-' + (x)).innerText;
 	// ex: note = Ab4 -> noteName = Ab; octave = 4;
 	var noteName = note.slice(0, -1);
@@ -214,4 +176,52 @@ function flip(){
 	else {
 		document.getElementById((y + 1) + '-' + (x)).innerText = result[0] + octave;
 	}
+}
+
+
+document.getElementById("share").addEventListener("click", share);
+function share(){
+	var msg = "";
+
+	var square_list = document.querySelectorAll('.roundedSquare');
+	var square_array = [...square_list]; // convert to array
+	square_array.forEach(square => {
+		let color = square.style.color;
+		if(color == colorCorrect){
+			msg = msg.concat('gren');
+			alert("gren");
+		}
+
+		/*
+		switch (square){
+			case colorCorrect:
+				msg = msg.concat('gren');
+				alert("gren");
+				break;
+			default:
+				msg += "gray";
+		}
+		*/
+	});
+	alert(msg);
+
+
+	/*
+	document.querySelectorAll('.roundedSquare').forEach(item => {
+		if(item.style.background == colorCorrect){
+			alert("green");
+		}
+
+
+		switch (item){
+			case colorCorrect:
+				msg += 'gren';
+				alert("gren");
+				break;
+			default:
+				msg += "gray";
+		}
+	});
+	*/
+
 }
